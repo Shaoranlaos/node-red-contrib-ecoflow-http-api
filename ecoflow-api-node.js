b34a65ab2155cce4582d9aa3400d3d8ed7733dd2
Shaoranlaos <shaoranlaos@shaoranlaos.de>
2024-07-24 19:20:50 +0200

    add query specific quotas

--- ecoflow-api-node.js
+++ ecoflow-api-node.js
@@ -14,17 +14,23 @@ module.exports = function(RED) {
 
             let func = fn ? fn : msg.function;
             let serialNumber = sn ? sn : msg.sn ? msg.sn : msg.payload;
+            let quotaTypes = msg.payload;
 
-            outFunc = function(data) {
-                
-            }
-
+            let data, error;
             switch(func) {
                 case 'queryQuotaAll':
                     [data,error] = await server.queryQuotaAll(serialNumber);
                     break;
                 case 'deviceList':
                     [data,error] = await server.queryDeviceList();
+                    break;
+                case 'queryQuotaSelective':
+                    node.debug(quotaTypes);
+                    if (getType(quotaTypes) != 'Array') {
+                        error = "msg.payload is not an array!";
+                    } else {
+                        [data,error] = await server.queryQuotaSelective(serialNumber,quotaTypes);
+                    }
                     break;
             }
 
@@ -33,10 +39,13 @@ module.exports = function(RED) {
                 send(msg);
                 done();
             } else {
+                //send(msg);
                 done(error);
             }
         });
     }
 
     RED.nodes.registerType("ecoflow-api", queryEcoflowApi);
+
+    const getType = obj => Object.prototype.toString.call(obj).slice(8, -1);
 }
